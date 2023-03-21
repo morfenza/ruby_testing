e051936121ed0ad3359a5f8788771aa3f3ed0bd5
Morfenza <ianlourenco@protonmail.com>
2023-03-21 14:20:52 -0300

    Do ex_11

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -36,16 +36,21 @@ end
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
 
+  subject(:aurora) { described_class.new('Aurora', 'Mixed', 'Grey') }
+
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'Cat has a shared method name with other classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(aurora).to_not be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(aurora).to be_hiding
   end
 end
